99925b9fd39222888f55893afa08f1e8998e0c70
Ravi Panchal <ravipanchal4125@gmail.com>
2026-07-11 01:36:53 +0530

    Event in ADD form, timers, and date events with small task

---
+++ Events/Form-Timers&Date/form.js
@@ -0,0 +1,36 @@
+// Submit Event & event.preventDefault()
+
+// submit.event: Triggered when a form is submitted
+// event.preventDefault(): Stops default form submission (page reload)
+
+// Get Value by Input Name in Submit Event
+
+// let userName = event.target.username.value
+
+// ======================= setTimtout() =======================
+// Runs a function once after a given delay (milliseconds)
+
+// setTimeout(() => {
+//     console.log("hello")
+// }, 2000)  // 2s
+
+// ======================= setInterval() =======================
+// Runs a function repeatedly after given time interval (milliseconds)
+
+// setInterval(() => {
+//     console.log("Byeeeee")
+// }, 1000)  // every 1s
+
+// ======================= Date Object =======================
+
+// let now = new Date(); // Current date & time
+
+// now.getFullYear(); // Year
+// now.getMonth(); // Month (0-11)
+// now.getDate(); // Day of month
+// now.getDay(); // Day of week (0-6)
+// now.getHours(); // Hours
+// now.getMinutes(); // Minutes
+// now.getSeconds(); // Seconds
+// now.toLocalDateString(); // Formatted date
+// now.toLocalTimeString(); // Formatted time
